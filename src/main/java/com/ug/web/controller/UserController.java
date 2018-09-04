e0dc7a8a035660a4aa4c88703168d85f9554328b
Ideal-Xiang <13723275355@163.com>
2018-09-04 16:34:36 +0800

    session小测试

--- src/main/java/com/ug/web/controller/UserController.java
+++ src/main/java/com/ug/web/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 import java.util.Objects;
 
@@ -42,12 +43,13 @@ public class UserController {
     //登陆
     @RequestMapping("/login.do")
     @ResponseBody
-    public ResultVo<User> userLogin(User user) {
+    public ResultVo<User> userLogin(User user, HttpSession session) {
         if (user != null) {
             User user1 = service.queryByUserName(user.getUsername());
             if (user1 != null) {
                 if (Objects.equals(user.getUsername(),user1.getUsername())) {
                     if (Objects.equals(user.getPassword(),user1.getPassword())) {
+                        session.setAttribute("user",user1);
                         System.out.println("登陆成功");
                         return ResultVo.setOK("登陆成功",null);
                     } else {
@@ -63,6 +65,14 @@ public class UserController {
             return ResultVo.setERROR("请输入账号和密码",null);
         }
     }
+
+    //获取用户信息
+    @RequestMapping("/uadd.do")
+    @ResponseBody
+    public ResultVo<User> updatePassWord(HttpSession session) {
+        return ResultVo.setOK("获取成功", session.getAttribute("user"));
+    }
+
 
     /*//修改密码
     @RequestMapping("/updatepw.do")
